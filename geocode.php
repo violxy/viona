1d79dcbb31e5aa7dc3437ba852e50e3cb19ae3e8
Viona Lam <viona_lam@hmc.edu>
2015-02-07 03:44:52 -0800

    More tinkering with php

--- geocode.php
+++ geocode.php
@@ -4,16 +4,16 @@ $username = "clinic";
 $password = "trickypassword";
 $dbname = "queries";
 
-$geocodeInfo['queryAddress'] = $_GET['queryAddress'];
-$geocodeInfo['formattedAddress'] = $_GET['formattedAddress'];
-$geocodeInfo['lat'] = $_GET['lat'];
-$geocodeInfo['lng'] = $_GET['lng'];
-$geocodeInfo['hasBounds'] = $_GET['hasBounds'];
-$geocodeInfo['NEBoundLat'] = $_GET['NEBoundLat'];
-$geocodeInfo['NEBoundLng'] = $_GET['NEBoundLng'];
-$geocodeInfo['SWBoundLat'] = $_GET['SWBoundLat'];
-$geocodeInfo['SWBoundLng'] = $_GET['SWBoundLng'];
-$geocodeInfo['foundGeocode'] = $_GET['foundGeocode'];
+$geocodeInfo['queryAddress'] = $_POST['queryAddress'];
+$geocodeInfo['formattedAddress'] = $_POST['formattedAddress'];
+$geocodeInfo['lat'] = $_POST['lat'];
+$geocodeInfo['lng'] = $_POST['lng'];
+$geocodeInfo['hasBounds'] = $_POST['hasBounds'];
+$geocodeInfo['NEBoundLat'] = $_POST['NEBoundLat'];
+$geocodeInfo['NEBoundLng'] = $_POST['NEBoundLng'];
+$geocodeInfo['SWBoundLat'] = $_POST['SWBoundLat'];
+$geocodeInfo['SWBoundLng'] = $_POST['SWBoundLng'];
+$geocodeInfo['foundGeocode'] = $_POST['foundGeocode'];
 
 print "queryAddress is $queryAddress";
  $conn = new mysqli($servername, $username, $password, $dbname);
